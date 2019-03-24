49fb28c06b392921846185c41f31cdcdec278aed
James E. Marca <james@activimetrics.com>
2019-03-24 12:19:11 -0700

    third one down

--- test/test_get_district.js
+++ test/test_get_district.js
@@ -1,29 +1,42 @@
-var get_district = require('../lib/get_district.js')
-var should = require('should')
-describe ('get district',function(){
-    it('should parse 2 digit districts from  vds filenames',function(){
-        var result = get_district('./files/vds_id.1205668.truck.imputed.2012.csv')
-        result.should.eql('d12')
-        return null
+const tap = require('tap')
+const get_district = require('../lib/get_district.js')
+const utils = require('./utils.js')
+
+tap.test('get district', async (t)=>{
+    t.test('should parse 2 digit districts from  vds filenames',async (tt)=>{
+        utils.promise_wrapper(get_district,'./files/vds_id.1205668.truck.imputed.2012.csv')
+            .then(result=>{
+                tt.equal(result,'d12')
+                return null
+            })
     })
-    it('should parse 2 digit districts from vds filenames',function(){
-        var result = get_district('vds_id.1205668.truck.imputed.2012.csv')
-        result.should.eql('d12')
-        return null
+    t.test('should parse 2 digit districts from vds filenames',async (tt)=>{
+        utils.promise_wrapper(get_district,'vds_id.1205668.truck.imputed.2012.csv')
+            .then(result=>{
+                tt.equal(result,'d12')
+                return null
+            })
     })
-    it('should parse one digit vds filenames',function(){
-        var result = get_district('./files/vds_id.305668.truck.imputed.2012.csv')
-        result.should.eql('d03')
-        return null
+    t.test('should parse one digit vds filenames',async (tt)=>{
+        utils.promise_wrapper(get_district,'./files/vds_id.305668.truck.imputed.2012.csv')
+            .then(result=>{
+                tt.equal(result,'d03')
+                return null
+            })
     })
-    it('should parse wim filenames',function(){
-        var result = get_district('./files/wim.114.W.truck.imputed.2012.csv')
-        result.should.eql('wim')
-        return null
+    t.test('should parse wim filenames',async (tt)=>{
+        utils.promise_wrapper(get_district,'./files/wim.114.W.truck.imputed.2012.csv')
+            .then(result=>{
+                tt.equal(result,'wim')
+                return null
+            })
     })
-    it('should parse wim filenames',function(){
-        var result = get_district('wim.114.W.truck.imputed.2012.csv')
-        result.should.eql('wim')
-        return null
+    t.test('should parse wim filenames',async (tt)=>{
+        utils.promise_wrapper(get_district,'wim.114.W.truck.imputed.2012.csv')
+            .then(result=>{
+                tt.equal(result,'wim')
+                return null
+            })
     })
+
 })
